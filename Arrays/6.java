3174ca158129ad2043e09fc084a58cc8d747a99b
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-04-07 20:09:32 +0530

    first commit

--- Arrays/6.java
+++ Arrays/6.java
@@ -1,77 +1,30 @@
 //Write a program to initialize an array and print them in a sorted order.
 
 
-package largestandSmallestArray;
-import java.util.Scanner;
-public class largeSmall {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scn = new Scanner (System.in);
-		System.out.print("Enter no. of elements you want in array:");
-                int n = scn.nextInt();
- 
-		int array[] = new int[n];
-                System.out.println("Enter all the elements:");
-		for (int i = 0; i < array.length; i++)
-		{
-			array[i] = scn.nextInt();
-		}
- 
-		int largest1, largest2, temp;
-		int smallest1, smallest2,flag;
- 
-		largest1 = array[0];
-		largest2 = array[1];
- 
-		if (largest1 < largest2)
-		{
-			temp = largest1;
-			largest1 = largest2;
-			largest2 = temp;
-		}
-		
-		smallest1 = array[0];
-		smallest2 = array[1];
- 
-		if (smallest1 > smallest2)
-		{
-			flag = smallest1;
-			smallest1 = smallest2;
-			smallest2 = flag;
-		}
- 
-		for (int i = 2; i < array.length; i++)
-		{
-			if (array[i] > largest1)
-			{
-				largest2 = largest1;
-				largest1 = array[i];
-			}
-			else if (array[i] > largest2 && array[i] != largest1)
-			{
-				largest2 = array[i];
-			}
-		}
- 
-		
-		for (int i = 2; i < array.length; i++)
-		{
-			if (array[i] < smallest1)
-			{
-				smallest2 = smallest1;
-				smallest1 = array[i];
-			}
-			else if (array[i] < smallest2 && array[i] != smallest1)
-			{
-				smallest2 = array[i];
-			}
-		}
-		System.out.println ("The First largest is " + largest1);
-		System.out.println ("The Second largest is " + largest2);
-		
-		System.out.println ("The First smallest is " + smallest1);
-		System.out.println ("The Second smalest is " + smallest2);
-	}
 
-}
+  class Solution {
+     public static void main(String[] args) {
+         int arr[] = { 0, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+          sort(arr);
+
+          for (int i = 0; i < arr.length; i++) {
+             System.out.print(arr[i] + " ");
+         }
+         System.out.println();
+     }
+
+      // selection sort
+     static void sort(int arr[]) {
+         for(int i=0; i<arr.length; i++) {
+             for(int j=i+1; j<arr.length; j++) {
+                 if(arr[j] < arr[i]) {
+                     int tmp = arr[i];
+                     arr[i] = arr[j];
+                     arr[j] = tmp;
+                 }
+             }
+         }
+     }
+ } 
