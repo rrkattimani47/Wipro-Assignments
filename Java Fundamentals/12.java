3b16ce2d7782bcac203ee6f0be53a2bbaf6eaea6
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-04-07 12:00:05 +0530

    first commit

---
+++ Java Fundamentals/12.java
@@ -0,0 +1,25 @@
+
+// Write a program to check if a given number is prime or not.
+
+public class Prime {
+
+    public static void main(String[] args) {
+
+        int num = 29;
+        boolean flag = false;
+        for(int i = 2; i <= num/2; ++i)
+        {
+            // condition for nonprime number
+            if(num % i == 0)
+            {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " is a prime number.");
+        else
+            System.out.println(num + " is not a prime number.");
+    }
+}
