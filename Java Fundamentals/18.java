072ebcac61d619749288beffa93bcb7858c1bdc4
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-04-07 11:01:29 +0530

    first commit

---
+++ Java Fundamentals/18.java
@@ -0,0 +1,25 @@
+//Check if the integer or string is palindrome or not
+
+import java.util.Scanner;
+public class palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str, rev = "";
+	      Scanner sc = new Scanner(System.in);
+	 
+	      System.out.println("Enter a string:");
+	      str = sc.nextLine();
+	 
+	      int length = str.length();
+	 
+	      for ( int i = length - 1; i >= 0; i-- )
+	         rev = rev + str.charAt(i);
+	 
+	      if (str.equals(rev))
+	         System.out.println(str+" is a palindrome");
+	      else
+	         System.out.println(str+" is not a palindrome");
+	}
+
+}
