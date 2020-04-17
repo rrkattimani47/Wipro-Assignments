e82b5d3744d4ac0f9fffc8f71ce656caf5e2b68f
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-04-17 13:11:16 +0530

    first commit

---
+++ Packages/3/Vehicle.java
@@ -0,0 +1,9 @@
+//Create abstract class Vehicle 
+
+package com.automobile;
+
+public abstract class Vehicle {
+	public abstract void getModelName();
+	public abstract void getRegistrationNumber();
+	public abstract void getOwnerName();
+}
