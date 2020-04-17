832d0505e530ac4fc57c4abb81be65a5fde0524c
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-04-17 13:15:42 +0530

    first commit

---
+++ Packages/3/Hero.java
@@ -0,0 +1,43 @@
+//Hero is subclass of Vehicle
+
+package com.automobile.twowheeler;
+import com.automobile.Vehicle;
+public class Hero extends Vehicle{
+	private String modelName;
+	private String registrationNumber;
+	private String ownerName;
+	private int speed;
+	
+	
+
+	public Hero(String modelName, String registrationNumber, String ownerName, int speed) {
+		super();
+		this.modelName = modelName;
+		this.registrationNumber = registrationNumber;
+		this.ownerName = ownerName;
+		this.speed = speed;
+	}
+
+	@Override
+	public void getModelName() {
+		System.out.println("ModelName: " + modelName);
+	}
+
+	@Override
+	public void getRegistrationNumber() {
+		System.out.println("RegistrationNumber: " + registrationNumber);
+	}
+
+	@Override
+	public void getOwnerName() {
+		System.out.println("OwnerName: " + ownerName);
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+	
+	public void radio() {
+		System.out.println("Accessing the radio");
+	}
+}
