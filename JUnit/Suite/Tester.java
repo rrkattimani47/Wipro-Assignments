483ee75c2f80f24301ab8cfb9d01872c5a78422b
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-04-20 15:29:51 +0530

    Create Tester.java

---
+++ JUnit/Suite/Tester.java
@@ -0,0 +1,20 @@
+
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	CalcTest.class,
+	StringmanipTest.class
+})
+
+public class Tester {
+	
+	public class AllTests {
+		
+	}
+}
