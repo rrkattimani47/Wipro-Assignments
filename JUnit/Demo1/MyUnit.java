83bd56bee24c58305b0ae550ab39b9f498942a9c
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-04-20 13:36:34 +0530

    Create MyUnit.java

---
+++ JUnit/Demo1/MyUnit.java
@@ -0,0 +1,10 @@
+/* Class Name: Demo1
+Method: String stringConcat(String,String)  [ returns concatenation of the 2 Strings received ]
+Create the above class and method and test it using JUnit.*/
+
+public class MyUnit {
+	public String stringConcat(String a, String b) {
+		return a + b;
+	}
+
+}
