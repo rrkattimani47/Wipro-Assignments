40bb8436f562e5d04869cf386003b1b48592c461
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-04-20 15:26:31 +0530

    Create CalcTest.java

---
+++ JUnit/Suite/CalcTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Test;
+
+public class CalcTest {
+
+	
+		Calc c = new Calc();
+
+		@Test
+		public void testAdd() {
+			assertEquals(5, c.add(10, -5));
+			assertEquals(5, c.add(10, -5));
+			assertEquals(5, c.add(20, -15));
+			assertEquals(5, c.add(0,5));
+		}
+
+		@Test
+		public void testSub() {
+			assertEquals(5, c.sub(10,5));
+			assertEquals(95, c.sub(100,5));
+			assertEquals(5, c.sub(20,15));
+			assertEquals(5,	c.sub(10,5));
+		}
+
+}
