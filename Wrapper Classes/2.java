1db18280b41846ce7a4dbc141a04360c07953e93
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-04-22 09:25:16 +0530

    Create 2.java

---
+++ Wrapper Classes/2.java
@@ -0,0 +1,24 @@
+/*Write a program to receive an integer number as a command line argument, and print the binary, octal and hexadecimal equivalent of the given number.
+
+Sample Output:
+
+java  Test 20
+Given Number :20
+Binary equivalent :10100
+Octal equivalent :24
+Hexadecimal equivalent :14*/
+
+//Run using Run configuration- arguments
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num = Integer.parseInt(args[0]);
+		
+		System.out.println("Given Number: " + num);		
+		System.out.println("Binary equivalent: " + Integer.toBinaryString(num));
+		System.out.println("Octal equivalent: " + Integer.toOctalString(num));
+		System.out.println("Hexadecimal equivalent: " + Integer.toHexString(num));
+	}
+
+}
