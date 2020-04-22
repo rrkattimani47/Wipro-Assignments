066ab9337a378369f302eb07d7f67ad66dffe772
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-04-22 14:20:15 +0530

    Update 2.java

--- classes and objects/2.java
+++ classes and objects/2.java
@@ -6,31 +6,25 @@ This method should return num1 to the power num2.
 3. Invoke both the methods and test the functionalities.
 Hint: Use Math.pow(double,double) to calculate the power.*/
 
-class Calci {
-     public static int powerInt(int num, int power) {
-         int res = 1;
-         for(int i = 0; i < power; i++) {
-             res *= num;
-         }
-         return res;
-     }
-     public static double powerDouble(double num,int power) {
-         double res = 1;
-         for(int i = 0; i < power; i++) {
-             res *= num;
-         }
-         return res;
-     }
- }
 
+	class Calci {
+    public static int powerInt(int num, int power) {
+        int res =(int)Math.pow(num,power);
+        return res;
+    }
+    public static double powerDouble(double num,int power) {
+    	double res =(double)Math.pow(num,power);
+        return res;
+    }
+}
 
-public class calculator {
+public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 System.out.println("Value of 2^10 : " + Calci.powerInt(2, 10));
-         System.out.println("Value of 3.14^10 : " + Calci.powerDouble(3.14, 10));
-     }
+		System.out.println("Value of 2^10 : " + Calci.powerInt(2, 10));
+        System.out.println("Value of 3.14^10 : " + Calci.powerDouble(3.14, 10));
 	}
 
+}
 
