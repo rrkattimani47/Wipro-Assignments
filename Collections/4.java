95098088020b6a2a785db8ec6603eca920426c85
Rashmi Kattimani <rrkattimani47@gmail.com>
2020-04-25 09:56:06 +0530

    Create 4.java

---
+++ Collections/4.java
@@ -0,0 +1,38 @@
+/*Create an ArrayList that can store only Strings. 
+Create a printAll method that will print all the elements of the ArrayList using an Iterator.*/
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+class MyArrayList<E> extends ArrayList<E> {
+	@Override
+	public boolean add(E e) {
+		if (e instanceof Integer || e instanceof Float || e instanceof Double) {
+			super.add(e);
+			return true;
+		} else {
+			throw new ClassCastException("Only Integer, Float, and Double are supported.");
+		}
+	}
+}
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+List<Object> list = new MyArrayList<>();
+		
+		try {
+			list.add(15);
+			list.add(1.2F);
+			list.add(3.1415D);
+			list.add("Test");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(list);
+
+	}
+
+}
